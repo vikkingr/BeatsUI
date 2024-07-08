e023f084bfc9a95d35e45a496ccb88b2bfb0b617
Robert Veinberg-Vikking <robivikking98@hotmail.com>
2024-07-08 18:37:55 -0400

    sample audio

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { BeatsContext } from '../../context/BeatsContext';
 import PlayPauseButton from '../../shared/PlayPauseButton/PlayPauseButton';
 import './PlayerControls.scss';
 
 const PlayerControls = () => {
     const { currentTrack, updateCurrentTrack } = useContext(BeatsContext);
+    //const audioCtxContainer = useRef();
 
     if (!currentTrack) {
         return <div>Loading</div>
@@ -13,18 +14,53 @@ const PlayerControls = () => {
     const { id, isPlaying } = currentTrack;
 
     const onClickPlayPause = () => {
-        const audioElement = document.getElementById("audio-element");
-        isPlaying ? audioElement.pause() : audioElement.play();
         updateCurrentTrack({ id: id, isPlaying: !isPlaying });
     }
+
+    // //
+    // audioCtxContainer.current = window.AudioContext || window.webkitAudioContext;
+    // const audioContext = new AudioContext();
+    // const audioElement = document.querySelector('#audio-element');
+    // const track = audioContext.createMediaElementSource(audioElement);
+
+    // const playButton = document.querySelector('play-pause-button');
+
+    // // play pause audio
+    // playButton.addEventListener('click', function () {
+
+    //     // check if context is in suspended state (autoplay policy)
+    //     if (audioContext.state === 'suspended') {
+    //         audioCtx.resume();
+    //     }
+
+    //     if (this.dataset.playing === 'false') {
+    //         audioElement.play();
+    //         this.dataset.playing = 'true';
+    //         // if track is playing pause it
+    //     } else if (this.dataset.playing === 'true') {
+    //         audioElement.pause();
+    //         this.dataset.playing = 'false';
+    //     }
+
+    //     let state = this.getAttribute('aria-checked') === "true" ? true : false;
+    //     this.setAttribute('aria-checked', state ? "false" : "true");
+
+    // }, false);
+
+    // // if track ends
+    // audioElement.addEventListener('ended', () => {
+    //     playButton.dataset.playing = 'false';
+    //     playButton.setAttribute("aria-checked", "false");
+    // }, false);
+    
+    // // connect our graph
+    // track.connect(audioContext.destination);
 
     return (
         <div className="player-controls">
             <span>PlayerControls</span>
-            <PlayPauseButton isPlaying={isPlaying} onClick={onClickPlayPause} />
-            <audio id="audio-element" controls>
-                <source src="../../assets/audio/newBurger.aif" />
-            </audio>
+            <PlayPauseButton id="play-pause-button" isPlaying={isPlaying} onClick={onClickPlayPause} />
+            <audio id="audio-element" src="src/assets/audio/newBurger.aif" controls></audio>
         </div>
     )
 }
