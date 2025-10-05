c4f96469d7b82b293ea2b2de9146b181f5b737de
Robert Veinberg-Vikking <robivikking98@hotmail.com>
2025-10-05 13:23:03 -0400

    audio and reconfig

--- src/context/BeatsContextProvider.jsx
+++ src/context/BeatsContextProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import beatService from "../services/beatService";
 import { BeatsContext } from "./BeatsContext";
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BeatsUI/',
+  base: "/",
+  build: {
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".wav"))
+            return "assets/audio/[name][extname]";
+          else
+            return "assets/[name].[hash][extname]";
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
