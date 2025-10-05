a7a945e71523a98b403bc3cc25aa88988eb1a35f
Robert Veinberg-Vikking <robivikking98@hotmail.com>
2025-10-05 17:23:13 -0400

    fix filepaths

--- src/services/beatService.js
+++ src/services/beatService.js
@@ -11,12 +11,17 @@ const beatService = {
             const parts = filePath.split('/');
             const fileName = parts[parts.length - 1];
             const name = fileName.replace('.wav', '');
+            
+            // Vite will copy assets to the correct place and update the import path to a URL
+            // So we can use the module's default export as the URL
             return {
                 id: idx + 1,
                 name,
                 isPlaying: false,
                 contributors: contributorsDefault,
-                src: filePath.replace('..', 'src'), // Vite will resolve this
+                src: audioFiles[filePath].default.startsWith('http')
+                  ? audioFiles[filePath].default
+                  : import.meta.env.BASE_URL + 'assets/audio/' + fileName,
             };
         });
     },
