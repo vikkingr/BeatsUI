dbfd3904195df1e0e22a1848e583cea64d612326
Robert Veinberg-Vikking <robivikking98@hotmail.com>
2025-10-05 15:43:40 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/BeatsUI/", 
   build: {
     assetsDir: "assets",
     rollupOptions: {
       output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
         assetFileNames: (assetInfo) => {
           if (assetInfo.name && assetInfo.name.endsWith(".wav"))
             return "assets/audio/[name][extname]";
-          else
-            return "assets/[name].[hash][extname]";
+          return "assets/[name][extname]";
         },
       },
     },
